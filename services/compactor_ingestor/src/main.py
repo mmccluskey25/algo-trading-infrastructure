3bab03dbda9a89f2e99551138387b29a93d1d67a
mmccluskey25 <mmccluskey25@pm.me>
2026-01-22 13:36:20 +0000

    Feat: Add compactor-ingestor microservice (#9)
    * initiate microservice folder
    
    * add compactor_ingestor code
    
    * prep for containerisation
    
    * refactor to allow CLI and add dockerfile
    
    * edit dockerfile for sleeping container
    
    * add service to docker-compose and align env variables and paths

---
+++ services/compactor_ingestor/src/main.py
@@ -0,0 +1,26 @@
+import os, time
+from core import compact_files
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DATA_DIR = os.getenv("DATA_DIR", "./data")
+LANDING_DIR = os.getenv("LANDING_DIR", "/data/landing/ticks")
+BRONZE_DIR = os.getenv("BRONZE_DIR", "/data/bronze/ticks")
+
+DELETE_RAW = os.getenv("DELETE_RAW", "true").lower() == "true"
+INTERVAL = int(os.getenv("COMPACT_INTERVAL", 240)) # default 4 hours
+
+def main():
+    print("Compactor-Ingestor scheduler started")
+    print(f"Interval: {INTERVAL} minutes.")
+    
+    while True:
+        compact_files(LANDING_DIR, BRONZE_DIR, delete_raw=DELETE_RAW)
+
+        print("Sleeping for {INTERVAL} minutes...")
+        time.sleep(INTERVAL * 60)
+
+
+if __name__ == "__main__":
+    main()

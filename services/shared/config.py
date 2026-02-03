88dbccff1dbeb21d1dcb3d7f97a3cd5239c6661f
Matt McCluskey <mmccluskey25@pm.me>
2026-02-03 15:10:00 +0000

    add shared env variable config file

---
+++ services/shared/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # container variables
+    prefix: str
+    host_data_path: str
+
+    # oanda listener
+    account_id: str
+    instruments: str
+
+    # redis
+    redis_port: int
+    redis_ext_port: int
+    redis_host: str
+    host_redis_path: str
+
+    # compactor-ingestor
+    delete_after_compaction: bool
+    compaction_interval_mins: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
+    )

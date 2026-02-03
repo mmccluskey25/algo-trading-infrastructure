153b403c9a389801df87707458daa03152e7c302
Matt McCluskey <mmccluskey25@pm.me>
2026-02-03 15:54:13 +0000

    refactor ingestor cli

--- services/compactor_ingestor/src/cli.py
+++ services/compactor_ingestor/src/cli.py
@@ -1,16 +1,16 @@
 import argparse, os
 from datetime import datetime
-from dotenv import load_dotenv, find_dotenv
-from core import compact_files
 
-load_dotenv(find_dotenv())
+from core import compact_files
+from services.shared.config import settings
+
 
 def main():
     parser = argparse.ArgumentParser(description="Manual trigger")
-    
+
     parser.add_argument("--delete", action="store_true", help="Delete raw data files after compaction")
-    parser.add_argument("--landing", default=os.getenv("LANDING_DIR", "/data/landing/ticks"))
-    parser.add_argument("--bronze", default=os.getenv("BRONZE_DIR", "/data/bronze/ticks"))
+    parser.add_argument("--landing", default=settings.landing_dir)
+    parser.add_argument("--bronze", default=settings.bronze_dir)
     parser.add_argument("--date", default=datetime.now().strftime("%Y%m%d"))
 
     args = parser.parse_args()

d8beb6611d5d62631c0ecd02d5842911a1cb70a7
Matt McCluskey <mmccluskey25@pm.me>
2026-02-25 15:04:08 +0000

    fix typo

--- services/orchestrator/src/orchestrator/definitions.py
+++ services/orchestrator/src/orchestrator/definitions.py
@@ -4,5 +4,5 @@ from orchestrator.assets.compaction import bronze_ticks, landing_ticks
 from orchestrator.resources import DataPathsResource
 
 defs = Definitions(
-    assets=[landing_ticks, bronze_ticks], resources={"data_paths": DataPathsResource}
+    assets=[landing_ticks, bronze_ticks], resources={"data_paths": DataPathsResource()}
 )

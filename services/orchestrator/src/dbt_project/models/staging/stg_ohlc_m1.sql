cb8d116f1e3fe5cc4165c24700c721c4d729a964
Matt McCluskey <mmccluskey25@pm.me>
2026-03-20 22:20:53 +0000

    refactor data model

--- services/orchestrator/src/dbt_project/models/staging/stg_ohlc_m1.sql
+++ services/orchestrator/src/dbt_project/models/staging/stg_ohlc_m1.sql
@@ -1,18 +1,18 @@
 select
     instrument,
-    time_bucket(interval '1 minute', tick_time) as candle_open,
-    first(mid order by tick_time) as open,
-    max(mid) as high,
-    min(mid) as low,
-    last(mid order by tick_time) as close,
-    first(bid order by tick_time) as bid_open,
-    max(bid) as bid_high,
-    min(bid) as bid_low,
-    last(bid order by tick_time) as bid_close,
-    first(ask order by tick_time) as ask_open,
-    max(ask) as ask_high,
-    min(ask) as ask_low,
-    last(ask order by tick_time) as ask_close
+    bucket as candle_open,
+    mid_open as open,
+    mid_high as high,
+    mid_low as low,
+    mid_close as close,
+    bid_open,
+    bid_high,
+    bid_low,
+    bid_close,
+    ask_open,
+    ask_high,
+    ask_low,
+    ask_close,
+    tick_count
 
-from {{ ref('int_ticks_unioned') }}
-group by instrument, candle_open
+from {{ source('forex', 'oanda_m1') }}

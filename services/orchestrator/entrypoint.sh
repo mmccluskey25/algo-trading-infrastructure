cfe3b9ccffdbe0e439bdf6271d4e9c9dc921b27e
mmccluskey25 <mmccluskey25@pm.me>
2026-04-30 12:02:24 +0100

    Feat: Add dagster sensor and job to trigger dbt build. (#45)

--- services/orchestrator/entrypoint.sh
+++ services/orchestrator/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Installing dbt packages..."
+dbt deps --project-dir /app/src/dbt_project --profiles-dir /app/src/dbt_project
+
 echo "Generating dbt manifest..."
 dbt parse --project-dir /app/src/dbt_project --profiles-dir /app/src/dbt_project
 
